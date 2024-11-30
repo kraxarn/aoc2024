be5b29962b37eb3bf9f621baff5483ff07afe31d
kraxarn <kraxie@protonmail.com>
2024-11-30 19:21:47 +0100

    Add custom function for reading files

--- main.c
+++ main.c
@@ -3,6 +3,41 @@
 
 #include "pocketpy.h"
 
+static bool read_lines(const int argc, py_Ref argv)
+{
+	PY_CHECK_ARGC(1);
+	PY_CHECK_ARG_TYPE(0, tp_str);
+
+	const char *path = py_tostr(py_arg(0));
+
+	FILE *file = fopen(path, "r");
+	if (file == nullptr)
+	{
+		return false;
+	}
+
+	char *line = nullptr;
+	size_t len = 0;
+	ssize_t read;
+
+	py_newlist(py_retval());
+
+	while ((read = getline(&line, &len, file)) != -1)
+	{
+		if (line[read - 1] == '\n')
+		{
+			line[read - 1] = '\0';
+		}
+
+		py_Ref reg = py_getreg(0);
+		py_newstr(reg, line);
+		py_list_append(py_retval(), reg);
+	}
+
+	fclose(file);
+	return true;
+}
+
 int main(const int argc, const char **argv)
 {
 	if (argc != 2)
@@ -32,6 +67,11 @@ int main(const int argc, const char **argv)
 	fread(buffer, 1, length, file);
 
 	py_initialize();
+
+	py_Ref reg = py_getreg(0);
+	py_newnativefunc(reg, read_lines);
+	py_setglobal(py_name("read_lines"), reg);
+
 	const bool ok = py_exec(buffer, "<string>", EXEC_MODE, nullptr);
 
 	if (!ok)
