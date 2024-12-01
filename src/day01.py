6feb8cd7f5ca8765dc7eef8473624ea02cb7b76d
kraxarn <kraxie@protonmail.com>
2024-12-01 11:13:02 +0100

    Day 1 - Part 2

--- src/day01.py
+++ src/day01.py
@@ -1,6 +1,7 @@
 lines: list[str] = read_lines("input/01")
 
 result1 = 0
+result2 = 0
 
 l_list: list[int] = []
 r_list: list[int] = []
@@ -16,4 +17,8 @@ r_list.sort()
 for i in range(len(l_list)):
 	result1 += abs(l_list[i] - r_list[i])
 
+for item in l_list:
+	result2 += item * r_list.count(item)
+
 print("Part 1:", result1)
+print("Part 2:", result2)
