02f0d6ca8a028ae760446e253c81b0ab20204f92
kraxarn <kraxie@protonmail.com>
2024-12-04 10:05:33 +0100

    Day 3 - Part 2

--- src/day03.py
+++ src/day03.py
@@ -1,11 +1,28 @@
 lines: list[str] = read_lines("input/03")
 
 result1 = 0
+result2 = 0
+
+def mul(value: str) -> int:
+	nums = [int(num) for num in value[value.index("(") + 1:value.index(")")].split(",")]
+	return nums[0] * nums[1]
 
 for line in lines:
 	matches: list[str] = re_find_all("mul\\([0-9]+,[0-9]+\\)", line)
 	for match in matches:
-		nums = [int(num) for num in match[match.index("(") + 1:match.index(")")].split(",")]
-		result1 += nums[0] * nums[1]
+		result1 += mul(match)
+
+enabled = True
+
+for line in lines:
+	matches: list[str] = re_find_all("do\\(\\)|don't\\(\\)|mul\\([0-9]+,[0-9]+\\)", line)
+	for match in matches:
+		if match == "do()":
+			enabled = True
+		elif match == "don't()":
+			enabled = False
+		elif enabled:
+			result2 += mul(match)
 
 print("Part 1:", result1)
+print("Part 2:", result2)
