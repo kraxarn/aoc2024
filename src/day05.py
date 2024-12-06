4daffe4744c547f61b8cdfc3a3da0d5e6bb85885
kraxarn <kraxie@protonmail.com>
2024-12-06 09:42:03 +0100

    Day 5 - Part 2

--- src/day05.py
+++ src/day05.py
@@ -3,6 +3,7 @@ lines: list[str] = read_lines("input/05")
 page_orders: list[list[int]] = []
 
 result1 = 0
+result2 = 0
 
 
 def is_valid(update: list[int]) -> bool:
@@ -14,6 +15,20 @@ def is_valid(update: list[int]) -> bool:
 	return True
 
 
+def swap(items: list[int], item1: int, item2: int) -> None:
+	items[item1], items[item2] = items[item2], items[item1]
+
+
+def make_valid(update: list[int]) -> bool:
+	for order in page_orders:
+		if (set(order) & set(update)) != set(order):
+			continue
+		if update.index(order[0]) > update.index(order[1]):
+			swap(update, update.index(order[0]), update.index(order[1]))
+			return True
+	return False
+
+
 for line in lines:
 	if "|" in line:
 		page_orders.append([int(idx) for idx in line.split("|")])
@@ -21,5 +36,10 @@ for line in lines:
 		nums = [int(idx) for idx in line.split(",")]
 		if is_valid(nums):
 			result1 += nums[len(nums) // 2]
+		else:
+			while make_valid(nums):
+				pass
+			result2 += nums[len(nums) // 2]
 
 print("Part 1:", result1)
+print("Part 2:", result2)
